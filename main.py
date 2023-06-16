0214bbafb84a7b2f1781f79702c8242b4540e71a
AshTron <ashutosh10615@gmail.com>
2023-06-16 19:24:40 +0530

    First Commit

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import cv2
 import os
 import numpy as np
 import open3d as o3d
-
 # Capture from Webcam
 cap=cv2.VideoCapture(0)
 cap.set(3, 1280)
@@ -36,95 +35,89 @@ while (True):
     cv2.imshow('Img', img)
     cv2.waitKey(1)
 
-    if success:
-        # if video is still left continue creating images
-        name = './data/frame' + str(currentframe) + '.jpg'
-        print('Creating...' + name)
 
-        # writing the extracted images
-        cv2.imwrite(name, img)
+    # if video is still left continue creating images
+    name = './data/frame' + str(currentframe) + '.jpg'
+    print('Creating...' + name)
 
-        # increasing counter so that it will
-        # show how many frames are created
+    # writing the extracted images
+    cv2.imwrite(name, img)
 
-        feature_extractor = GLPNImageProcessor.from_pretrained("vinvino02/glpn-nyu")
-        model = GLPNForDepthEstimation.from_pretrained("vinvino02/glpn-nyu")
+    # increasing counter so that it will
+    # show how many frames are created
 
-        # load and resize the input image
-        image = Image.open("data/frame"+str(currentframe)+".jpg")
-        new_height = 480 if image.height > 480 else image.height
-        new_height -= (new_height % 32)
-        new_width = int(new_height * image.width / image.height)
-        diff = new_width % 32
-        new_width = new_width - diff if diff < 16 else new_width + 32 - diff
-        new_size = (new_width, new_height)
-        image = image.resize(new_size)
+    feature_extractor = GLPNImageProcessor.from_pretrained("vinvino02/glpn-nyu")
+    model = GLPNForDepthEstimation.from_pretrained("vinvino02/glpn-nyu")
 
-        # prepare image for the model
-        inputs = feature_extractor(images=image, return_tensors="pt")
+    # load and resize the input image
+    image = Image.open("data/frame"+str(currentframe)+".jpg")
+    new_height = 480 if image.height > 480 else image.height
+    new_height -= (new_height % 32)
+    new_width = int(new_height * image.width / image.height)
+    diff = new_width % 32
+    new_width = new_width - diff if diff < 16 else new_width + 32 - diff
+    new_size = (new_width, new_height)
+    image = image.resize(new_size)
 
-        # get the prediction from the model
-        with torch.no_grad():
-            outputs = model(**inputs)
-            predicted_depth = outputs.predicted_depth
+    # prepare image for the model
+    inputs = feature_extractor(images=image, return_tensors="pt")
 
-        # remove borders
-        pad = 16
-        output = predicted_depth.squeeze().cpu().numpy() * 1000.0
-        output = output[pad:-pad, pad:-pad]
-        image = image.crop((pad, pad, image.width - pad, image.height - pad))
+    # get the prediction from the model
+    with torch.no_grad():
+        outputs = model(**inputs)
+        predicted_depth = outputs.predicted_depth
 
-        # visualize the prediction
-        fig, ax = plt.subplots(1, 2)
-        ax[0].imshow(image)
-        ax[0].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-        ax[1].imshow(output, cmap='plasma')
-        ax[1].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-        plt.tight_layout()
-        plt.pause(5)
-        currentframe += 1
+    # remove borders
+    pad = 16
+    output = predicted_depth.squeeze().cpu().numpy() * 1000.0
+    output = output[pad:-pad, pad:-pad]
+    image = image.crop((pad, pad, image.width - pad, image.height - pad))
 
-        width, height = image.size
+    # visualize the prediction
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(image)
+    ax[0].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+    ax[1].imshow(output, cmap='plasma')
+    ax[1].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+    plt.tight_layout()
+    plt.pause(5)
+    currentframe += 1
 
-        depth_image = (output * 255 / np.max(output)).astype('uint8')
-        image = np.array(image)
+    width, height = image.size
 
-        # create rgbd image
-        depth_o3d = o3d.geometry.Image(depth_image)
-        image_o3d = o3d.geometry.Image(image)
-        rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(image_o3d, depth_o3d,
-                                                                        convert_rgb_to_intensity=False)
+    depth_image = (output * 255 / np.max(output)).astype('uint8')
+    image = np.array(image)
 
-        # camera settings
-        camera_intrinsic = o3d.camera.PinholeCameraIntrinsic()
-        camera_intrinsic.set_intrinsics(width, height, 500, 500, width / 2, height / 2)
+    # create rgbd image
+    depth_o3d = o3d.geometry.Image(depth_image)
+    image_o3d = o3d.geometry.Image(image)
+    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(image_o3d, depth_o3d,
+                                                                    convert_rgb_to_intensity=False)
 
-        # create point cloud
-        pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, camera_intrinsic)
+    # camera settings
+    camera_intrinsic = o3d.camera.PinholeCameraIntrinsic()
+    camera_intrinsic.set_intrinsics(width, height, 500, 500, width / 2, height / 2)
 
-        # outliers removal
-        cl, ind = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=20.0)
-        pcd = pcd.select_by_index(ind)
+    # create point cloud
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, camera_intrinsic)
 
-        # estimate normals
-        pcd.estimate_normals()
-        pcd.orient_normals_to_align_with_direction()
+    # outliers removal
+    cl, ind = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=20.0)
+    pcd = pcd.select_by_index(ind)
 
-        # surface reconstruction
-        mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=8, n_threads=1)[0]
+    # estimate normals
+    pcd.estimate_normals()
+    pcd.orient_normals_to_align_with_direction()
 
-        # rotate the mesh
-        rotation = mesh.get_rotation_matrix_from_xyz((np.pi, 0, 0))
-        mesh.rotate(rotation, center=(0, 0, 0))
+    # surface reconstruction
+    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=8, n_threads=1)[0]
 
-        # save the mesh
-        o3d.io.write_triangle_mesh(f'./mesh.obj', mesh)
+    # rotate the mesh
+    rotation = mesh.get_rotation_matrix_from_xyz((np.pi, 0, 0))
+    mesh.rotate(rotation, center=(0, 0, 0))
 
-        # visualize the mesh
-        o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
-    else:
-        break
+    # save the mesh
+    o3d.io.write_triangle_mesh(f'./mesh.obj', mesh)
 
-# Release all space and windows once done
-cap.release()
-cv2.destroyAllWindows()
+    # visualize the mesh
+    o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
